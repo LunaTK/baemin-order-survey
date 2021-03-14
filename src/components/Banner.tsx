6032f8f73ca206b00401264ec093f7847dea8729
LunaTK <ahn_v3@naver.com>
2021-03-14 14:21:43 +0900

    feat: banner to display mock status

---
+++ src/components/Banner.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Banner = () => (<div style={{
+  textAlign: 'center',
+  color: 'yellow',
+  backgroundColor: 'gray',
+  position: 'fixed',
+  width: '100%',
+  zIndex: 1,
+}}>
+  현재 보시는 페이지는 데모용 사이트로, Mocking된 데이터가 표시됩니다
+</div>);
+
+export default Banner;
--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker'
+import Banner from './components/Banner';
 
 ReactDOM.render(
   <React.StrictMode>
+  {process.env.REACT_APP_USE_MOCK && <Banner />}
     <App />
   </React.StrictMode>,
   document.getElementById('root')
