dbb13f9325edc30edfdba3962ed8e409daf13fdc
LunaTK <ahn_v3@naver.com>
2021-04-12 18:06:54 +0900

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/baemin-order-survey/precache-manifest.49e52f749ed870d8fea616eb706588ed.js"
+  "/baemin-order-survey/precache-manifest.327175da32f30e6be42718e0a50d2e58.js"
 );
 
 self.addEventListener('message', (event) => {
