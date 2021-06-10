732ded9456f524979218e8fc38f567d372823538
LunaTK <ahn_v3@naver.com>
2021-06-10 14:26:41 +0900

    refactor: invalid indenting

--- src/components/SelectedMenuPreview/SelectedMenuPreview.tsx
+++ src/components/SelectedMenuPreview/SelectedMenuPreview.tsx
@@ -15,22 +15,22 @@ type SelectedMenuPreviewProps = {
 const SelectedMenuPreview: React.FC<SelectedMenuPreviewProps> = ({
   selectedMenu, index, removeOrder, showFullOption,
 }) => (
-    <div className="selected-menu-preview">
-      { !!removeOrder && <Button
-        type="text" size="small" shape="circle-outline"
-        icon={<CloseOutlined />}
-        onClick={() => removeOrder && removeOrder(index!)}
-      /> }
-      <div className="selected-menu-preview-detail">
-        <div className="title">
-          {!!(selectedMenu.menuDefault.length > 0) && `[${selectedMenu.menuDefault}] `}
-          {selectedMenu.menuName} - {selectedMenu.totalPrice} 원
-        </div>
-        <div className="option" style={{ whiteSpace: showFullOption ? 'normal' : 'nowrap' }}>
-          {Object.values(selectedMenu.options).flatMap((x) => x).join(', ')}
-        </div>
+  <div className="selected-menu-preview">
+    { !!removeOrder && <Button
+      type="text" size="small" shape="circle-outline"
+      icon={<CloseOutlined />}
+      onClick={() => removeOrder && removeOrder(index!)}
+    /> }
+    <div className="selected-menu-preview-detail">
+      <div className="title">
+        {!!(selectedMenu.menuDefault.length > 0) && `[${selectedMenu.menuDefault}] `}
+        {selectedMenu.menuName} - {selectedMenu.totalPrice} 원
+      </div>
+      <div className="option" style={{ whiteSpace: showFullOption ? 'normal' : 'nowrap' }}>
+        {Object.values(selectedMenu.options).flatMap((x) => x).join(', ')}
       </div>
     </div>
+  </div>
 );
 
 export default SelectedMenuPreview;
