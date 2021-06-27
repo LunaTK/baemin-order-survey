a09fd3881e49b3bf6b0abb89ced1bf16fc6f46a0
LunaTK <ahn_v3@naver.com>
2021-06-27 16:49:31 +0900

    feat: use modular import with firebase-v9

--- src/types/common.ts
+++ src/types/common.ts
@@ -1,11 +1,6 @@
-/// <reference types="firebase/app" />
 /**
  * This file includes types generally used across the project
  */
-
-declare global {
-  const firebase: typeof firebase;
-}
 
 export type FirestoreDocRef<T = any> = {
   id: string;
